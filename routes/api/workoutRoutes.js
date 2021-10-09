c0514edf56c79d17f9b00d759e737f3d30dcfa24
Matilda Brantley <matildahbrantley@gmail.com>
2021-10-08 20:17:27 -0400

    Fixed POST request for creating new workout.

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -3,11 +3,14 @@ const models = require("../../models");
 const router = require('express').Router();
 
 //New workout POST request
-router.post('/api/workouts/', (req, res) => {
-    console.log(req.body)
-        models.Workout.create(req.body)
-          .then((db) => res.json(db))
-          .catch(err => res.status(400).json(err));
+router.post('/api/workouts', (req, res) => {
+  models.Workout.create({})
+    .then((db) => {
+      res.json(db);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 //New exercise PUT request
